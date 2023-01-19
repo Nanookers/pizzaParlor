edc87677956cc751dc07f43382436f028a60a8d9
Connor Clark <connordeclark@gmail.com>
2023-01-19 16:37:01 -0600

    POST goes through server, rejects in db

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import axios from 'axios';
 import './App.css';
+import Form from './Form/Form';
+import { HashRouter as Router, Route, Link } from 'react-router-dom';
 
 function App() {
 
   return (
+    <Router>
     <div className='App'>
       <header className='App-header'>
         <h1 className='App-title'>Prime Pizza</h1>
@@ -12,8 +15,11 @@ function App() {
   
       <img src='images/pizza_photo.png' />
       <p>Pizza is great.</p>
-  
+      <Route exact path="/Form">
+        <Form />
+      </Route>
     </div>
+    </Router>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -3,4 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App/App';
 
+// const reduxStore = createStore(
+//     combineReducers({
+//       pizzaList
+//     }),
+//     applyMiddleware(logger)
+//   );
+
 ReactDOM.render(<App />, document.getElementById('root'));
