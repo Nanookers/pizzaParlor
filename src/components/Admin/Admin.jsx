c6f3d4f710739846e50c8b1c295f3d287bf1a2af
Aman Abdirazak <amanabdirizak@icloud.com>
2023-01-20 11:04:30 -0600

    need to merge to finish Admin page

--- src/components/Admin/Admin.jsx
+++ src/components/Admin/Admin.jsx
@@ -1,10 +1,61 @@
+import axios from "axios";
+import { useEffect } from "react";
+import { useSelector, useDispatch } from 'react-redux';
+
 function Admin() {
+
+    useEffect(() => {
+        //when you go to /admin, all the previous orders should be fetched with a get request right away
+        showPreviousOrders();
+    }, []) 
+
+    //calling adminOrders array from redux story
+    const adminOrders = useSelector(store => store.adminOrders);
+    const dispatch = useDispatch();
+
+    const showPreviousOrders = () => {
+        axios({
+            method: 'GET',
+            url: '/api/order'
+        })
+        .then((response) => {
+            console.log(response.data);
+            const action = {
+                type: 'GET_ADMIN_ORDERS',
+                payload: response.data
+            }
+            dispatch(action);
+        })
+        .catch((error) => {
+            console.error('error in get on admin page', error);
+        })
+    }
+
     return (
-        <table>
-            <thead>
-                
-            </thead>
-        </table>
+        <>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Name</th>
+                        <th>Time Order Placed</th>
+                        <th>Type</th>
+                        <th>Cost</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {adminOrders.map((orders, i) => {
+                        return (
+                            <tr key={orders[i]}>
+                                <td>{orders[i].customer_name}</td>
+                                <td>{orders[i].time}</td>
+                                <td>{orders[i].type}</td>
+                                <td>{orders[i].total}</td>
+                            </tr>
+                        )
+                    })}
+                </tbody>
+            </table>
+        </>
     )
 }
 
--- src/index.js
+++ src/index.js
@@ -25,10 +25,22 @@ const pizzaCart = (state=[{id: 1, order_id: 1, pizza_id: 1}], action ) => {
     return state;
 }
 
+//orders for admin
+const adminOrders = (state = [], action) => {
+    console.log('on admin page')
+    if (action.type === 'GET_ADMIN_ORDERS') {
+        //return a copy of origin array since this array can't be mutated
+        //this copy of the array can be added to/changed
+        return [...state, action.payload]
+    }
+    return state;
+}
+ 
 const reduxStore = createStore(
     combineReducers({
         pizzaList,
-        pizzaCart
+        pizzaCart, 
+        adminOrders
     }),
     applyMiddleware(logger)
 );
